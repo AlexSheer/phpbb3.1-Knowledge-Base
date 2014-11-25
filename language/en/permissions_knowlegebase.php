df17d8ef22576db1db4f23533e9313aa0f49d757
AlexSheer <alex_sheer@inbox.lv>
2014-11-25 14:25:15 +0200

    Create permissions_knowlegebase.php

---
+++ language/en/permissions_knowlegebase.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Extension - Knowlege Base
+* @copyright (c) 2013 phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_A_MANAGE_KB'		=> 'Can manage Kowlege Base',
+));
